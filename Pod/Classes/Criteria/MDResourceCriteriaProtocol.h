1133b74e1957171a858b36b9a9e6598a24128de6
Joao Prudencio <joao.prudencio@mindera.com>
2015-02-24 23:34:37 +0000

    documenting resource criteria protocol

--- Pod/Classes/Criteria/MDResourceCriteriaProtocol.h
+++ Pod/Classes/Criteria/MDResourceCriteriaProtocol.h
@@ -12,23 +12,29 @@
 
 @required
 
-/** @brief Validate if the criteria is responsible of handling this qualifier.
+/**
+ *  Validate if the criteria is responsible of handling this qualifier.
  *  e.g. Orientation criteria should only accept landscape/portrait qualifiers.
  *  @param qualifier string that represents a criteria
+ *  @return YES if criteria can handle the qualifier
  */
 - (BOOL)respondsToQualifier:(NSString *)qualifier;
 
-/** @brief Validate if qualifier match the criteria configuration.
+/**
+ *  Validate if qualifier match the criteria configuration.
  *  e.g. Orientation criteria should return NO if the device is in landscape and the qualifier is portrait.
  *  @param qualifier string that represents a criteria
+ *  @return return YES if the qualifier meets the criteria
  */
 - (BOOL)meetCriteriaWith:(NSString *)qualifier;
 
-/** @brief Validate if the qualifier2 is more important than qualifier1.
+/**
+ *  Validate if the qualifier2 is more important than qualifier1.
  *  e.g. A device width criteria should return YES if qualifier1=200 qualifier2=400 deviceWidth=400
  *  Since the qualifier2 is closer to the actual device configuration.
  *  @param qualifier1 to be overriden
  *  @param qualifier2 that will override qualifier1
+ *  @return YES if qualifier2 should override qualifier1
  */
 - (BOOL)shouldOverrideQualifier:(NSString *)qualifier1 withQualifier:(NSString *)qualifier2;
 
